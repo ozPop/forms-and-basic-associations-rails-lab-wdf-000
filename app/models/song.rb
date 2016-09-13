9f1f30a2319033349030ca7a73fa7de9d2288a4d
ozPop <ozziepopovas@gmail.com>
2016-09-13 14:30:45 -0400

    add genre & artist custom setters

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,25 @@
 class Song < ActiveRecord::Base
+
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  # ask question about why using attr_reader doesnt work?
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
 end
 
