512b5d974fdfa7e4e7180965499be2a0b9297e20
ozPop <ozziepopovas@gmail.com>
2016-09-13 15:17:47 -0400

    refactor song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
 
   # ask question about why using attr_reader doesnt work?
   def genre_name
-    self.genre.name
+    self.genre.name unless self.genre.blank?
   end
 
   def artist_name=(name)
@@ -18,7 +18,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name
-    self.artist.name
+    self.artist.name unless self.artist.blank?
   end
 
   def note_contents=(contents)
@@ -28,7 +28,7 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents
-    self.notes.collect {|note| note.content}
+    self.notes.collect {|note| note.content} unless self.notes.blank?
   end
 
 end
